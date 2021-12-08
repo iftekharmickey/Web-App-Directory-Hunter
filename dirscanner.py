09486fe570d89ada4616d8648f598f9db309acef
iftekharmickey <66146458+iftekharmickey@users.noreply.github.com>
2021-12-08 11:03:10 +0600

    Create dirscanner.py

---
+++ dirscanner.py
@@ -0,0 +1,18 @@
+import requests
+
+target_url = input('[*] Enter Target URL: ')
+file_name = input('[*] Enter List Containing Directories: ')
+
+def request(url):
+	try:
+		return requests.get("http://" + url)
+	except requests.exceptions.ConnectionError:
+		pass
+  
+file = open(file_name, 'r')
+for line in file:
+	directory = line.strip()
+	full_url = target_url + '/' + directory
+	response = request(full_url)
+	if response:
+		print('[*] Discovered Directory: ' + full_url)
